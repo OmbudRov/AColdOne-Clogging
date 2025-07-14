3e16af798e9605ecef61cfd81369e2f8755e9feb
OmbudRov <66506616+OmbudRov@users.noreply.github.com>
2025-07-14 20:48:06 +0530

    Update AudioTriggers.java

--- src/main/java/com/acoldoneclogging/AudioTriggers.java
+++ src/main/java/com/acoldoneclogging/AudioTriggers.java
@@ -3,14 +3,12 @@ package com.acoldoneclogging;
 import java.util.Random;
 import java.util.regex.Pattern;
 import javax.inject.Inject;
-import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.events.ActorDeath;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.client.eventbus.Subscribe;
 
-@Slf4j
 public class AudioTriggers
 {
 	@Inject
